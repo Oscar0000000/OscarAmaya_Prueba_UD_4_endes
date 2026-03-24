01b63033d2a76f57dcddff4bb6eada8c7d03cb31
Oscar0000000 <oscaramcort13@gmail.com>
2026-03-24 10:07:40 +0100

    Add files via upload

---
+++ OscarAmaya_Ej2-Pru_UD4/src/main/java/com/mycompany/oscaramaya_ej2/pru_ud4/OscarAmaya_Ej2Pru_UD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oscaramaya_ej2.pru_ud4;
+
+/**
+ *
+ * @author 13_1DAW-Alum
+ */
+public class OscarAmaya_Ej2Pru_UD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
