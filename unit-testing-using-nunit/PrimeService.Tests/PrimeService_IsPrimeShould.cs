961c80024d4a839ebcd3610810bdb271c1bbf13e
Oscar0000000 <oscaramcort13@gmail.com>
2026-03-24 09:44:53 +0100

    Add files via upload

---
+++ unit-testing-using-nunit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NUnit.Framework;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public bool IsPrime(int candidate)
+
+        if (candidate <==1)
+        return false;
+    public class PrimeService_IsPrimeShould
+    {
+        private PrimeService _primeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
